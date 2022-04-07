562ae28270bdf8ec724fd4aae6a05271b25660be
Adam Knieć <adam.kniec@netguru.com>
2022-04-07 22:13:24 +0200

    refactoring the stories to use a template

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -1,6 +1,26 @@
-// export const Button = () => <button>Kliks</button>;
+import { Button } from "./Button";
 
-// export default {
-//   title: "Nasz pierwszy testowy button",
-//   component: Button,
-// };
+export default {
+  component: Button,
+  title: "Components/Button",
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  background: "#000",
+  label: "Klik",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  background: "#2233",
+  label: "Continue",
+};
+
+export const Fancy = Template.bind({});
+Fancy.args = {
+  background: "#da2",
+  label: "Submit",
+};
